32d329e7167ac5c7aa94408789890d136399e417
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2025-05-26 03:38:45 +0700

    Rename amounts and attach alphabet button script

---
+++ Assets/Scripts/AlphabetButton.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlphabetButton : MonoBehaviour
+{
+    public string letter;
+    public int amount = 0;
+    public TMP_Text amountText;
+
+    public void SetAmount(int newAmount)
+    {
+        amount = newAmount;
+        amountText.text = amount.ToString();
+    }
+
+    public bool TryUseLetter()
+    {
+        if (amount > 0)
+        {
+            amount--;
+            amountText.text = amount.ToString();
+            return true;
+        }
+        return false;
+    }
+}
